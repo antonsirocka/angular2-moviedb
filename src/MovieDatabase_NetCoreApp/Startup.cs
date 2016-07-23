06760f342660ba810e073c96a37f6a4f757a3fbf
antonsirocka <anton.sirocka@gmail.com>
2016-07-23 16:04:36 +1000

    Add files via upload

---
+++ src/MovieDatabase_NetCoreApp/Startup.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using MovieDatabase_NetCoreApp.Repositories;
+using Microsoft.EntityFrameworkCore;
+using MovieDatabase_NetCoreApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Net;
+
+namespace MovieDatabase_NetCoreApp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MovieDatabase")));
+
+            services.AddDbContext<LoginDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("IdentityDatabase")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(o =>
+            {
+                o.Password.RequireDigit = false;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 1;
+                o.Cookies.ApplicationCookie.LoginPath = null;
+
+                o.Cookies.ApplicationCookie.Events = new CookieAuthenticationEvents
+                {
+                    OnRedirectToLogin = ctx =>
+                    {
+                        if (ctx.Request.Path.StartsWithSegments("/api") &&
+                            (ctx.Response.StatusCode == (int)HttpStatusCode.OK || ctx.Response.StatusCode == (int)HttpStatusCode.Unauthorized))
+                        { 
+                            ctx.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        }
+                        else
+                        {
+                            ctx.Response.Redirect(ctx.RedirectUri);
+                        }
+
+                        return Task.FromResult(0);
+                    }
+                };
+            })
+            .AddEntityFrameworkStores<LoginDbContext>()
+            .AddDefaultTokenProviders();
+
+            services.AddOptions();
+
+            services.Configure<AppSettings>(appSettings =>
+            {
+                appSettings.SecretKey = Configuration.GetSection("AppSettings")["SecretKey"];
+            });
+
+            services.AddScoped<IMovieRepositoryAsync, MovieRepositoryAsync>();
+            services.AddScoped<IMovieRatingRepository, MovieRatingRepository>();
+            services.AddScoped<IVotesRepositoryAsync, VotesRepositoryAsync>();
+            services.AddScoped<IMovieContext, MovieContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseStatusCodePages();
+
+            app.UseIdentity();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" }
+                    );
+            });
+        }
+    }
+}
